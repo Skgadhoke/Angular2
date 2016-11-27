6d7651c2f2361b8a096a78e2cae65c56a22b1fc3
Skgadhoke <sim.gadhoke@gmail.com>
2016-11-27 02:19:35 -0600

    in here has a function called onEnter which takes in a string. This function filters the list of crimes by IUCR.

--- angular-crime-analysis/app/app.component.ts
+++ angular-crime-analysis/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NgGridModule } from 'angular2-grid';
 import { CrimeCodes } from './crimeCode';
+import { CrimeInfo } from './crimes';
 
 const CODES: CrimeCodes[] = [
- { id: 1025, desc1: 'ARSON', desc2: 'AGGRAVATED'},
- { id: 1090, desc1: 'ARSON', desc2: 'ATTEMPT ARSON'},
- { id: 1010, desc1: 'ARSON', desc2: 'BY EXPLOSIVE '},
- { id: 1020, desc1: 'ARSON', desc2: 'BY FIRE '},
- { id: 1030, desc1: 'ARSON', desc2: 'POS: CHEMICAL/DRY-ICE DEVICE'}
+ { id: '1025', desc1: 'ARSON', desc2: 'AGGRAVATED'},
+ { id: '1090', desc1: 'ARSON', desc2: 'ATTEMPT ARSON'},
+ { id: '1010', desc1: 'ARSON', desc2: 'BY EXPLOSIVE '},
+ { id: '1020', desc1: 'ARSON', desc2: 'BY FIRE '},
+ { id: '1030', desc1: 'ARSON', desc2: 'POS: CHEMICAL/DRY-ICE DEVICE'}
 
+];
+
+const CRIMES: CrimeInfo[] = [
+  { IUCR: '1025', domestic: 'FALSE', arrest: 'FALSE'},
+  { IUCR: '1025', domestic: 'TRUE', arrest: 'FALSE'},
+  { IUCR: '1020', domestic: 'FALSE', arrest: 'TRUE'},
 ];
 
 @Component({
@@ -18,7 +25,27 @@ const CODES: CrimeCodes[] = [
 export class AppComponent 
 {
   codes = [];
+  crimeInfo = [];
+  codeName = '';
+  value = '';
+  disp = 0;
+
+    onEnter(value: string) {
+	this.value = value; 
+	this.codes = CODES;
+	this.crimeInfo = CRIMES;
+	this.crimeInfo = CRIMES.filter(ci => ci.IUCR === value);
+	this.disp = 1;
+    }
+
+  searchIUCR(cName: string){
+    this.codes = CODES;
+    this.codeName = cName;
+    this.value = cName;
+  }
+
   dispCrimeCodes(){
     this.codes = CODES; 
+    this.disp = 2;
   }
 }
